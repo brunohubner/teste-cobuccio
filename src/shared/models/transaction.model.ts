cea331578e7c5afb684b022abffec5cf2f997e61
Bruno Hubner <brunohubnerdev@gmail.com>
2025-02-20 23:58:33 -0300

    feat: Add Transaction model with attributes

---
+++ src/shared/models/transaction.model.ts
@@ -0,0 +1,70 @@
+import { DataTypes } from 'sequelize';
+import {
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey,
+  Model,
+  Scopes,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+import User from './user.model';
+
+@Scopes(() => ({}))
+@Table({ schema: 'public', tableName: 'transaction', updatedAt: false })
+export default class Transaction extends Model<Transaction> {
+  @Column({
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+    type: DataType.UUID,
+  })
+  declare id: string;
+
+  @ForeignKey(() => User)
+  @Column
+    sender_id: string;
+
+  @BelongsTo(() => User)
+    sender: User;
+
+  @ForeignKey(() => User)
+  @Column
+    receiver_id: string;
+
+  @BelongsTo(() => User)
+    receiver: User;
+
+  @Column({
+    type: DataTypes.DECIMAL(10, 6),
+    allowNull: false,
+  })
+    amount: string;
+
+  @Column({
+    allowNull: false,
+    type: DataType.STRING,
+  })
+    status: string;
+
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+    previousHash: string;
+
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+    hash: string;
+
+  @CreatedAt
+  @Column
+    created_at: Date;
+
+  @UpdatedAt
+  @Column
+    updated_at: Date;
+}

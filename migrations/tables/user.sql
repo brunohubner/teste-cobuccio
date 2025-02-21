13ea7842e5093437eae40c76a2a5935620553b38
Bruno Hubner <brunohubnerdev@gmail.com>
2025-02-20 21:15:56 -0300

    feat: Add uuid generation function and user table creation

---
+++ migrations/tables/user.sql
@@ -0,0 +1,15 @@
+-- DROP TABLE IF EXISTS public.user;
+CREATE TABLE IF NOT EXISTS public.user (
+	id character varying(36) COLLATE pg_catalog."default" NOT NULL DEFAULT public.uuid_generate_v4(),
+	person_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+	email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+	cpf character varying(11) COLLATE pg_catalog."default" NOT NULL,
+	hashed_password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+	birth_date date NOT NULL,
+	created_at timestamp without time zone NOT NULL DEFAULT now(),
+	updated_at timestamp without time zone NOT NULL DEFAULT now(),
+	CONSTRAINT user_pkey PRIMARY KEY (id)
+) TABLESPACE pg_default;
+
+ALTER TABLE
+	IF EXISTS public.user OWNER to "MasterPostgres";

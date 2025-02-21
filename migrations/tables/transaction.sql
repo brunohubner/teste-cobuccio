a1855eda116bdc64f53705098dfd12cc7842a371
Bruno Hubner <brunohubnerdev@gmail.com>
2025-02-20 23:54:51 -0300

    feat: Add transaction table to database

---
+++ migrations/tables/transaction.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS public.transaction;
+
+CREATE TABLE IF NOT EXISTS public.transaction (
+	id character varying(36) COLLATE pg_catalog."default" NOT NULL DEFAULT public.uuid_generate_v4(),
+	sender_id character varying(36) COLLATE pg_catalog."default" NOT NULL,
+	receiver_id character varying(36) COLLATE pg_catalog."default" NOT NULL,
+	amount numeric(10,6) NOT NULL,
+	status character varying(20) CHECK (status IN ('pending', 'completed', 'cancelled', 'rejected')) NOT NULL DEFAULT 'pending',
+	previousHash character varying(64) COLLATE pg_catalog."default" NOT NULL,
+	hash character varying(64) COLLATE pg_catalog."default" NOT NULL,
+	created_at timestamp without time zone NOT NULL DEFAULT now(),
+	updated_at timestamp without time zone NOT NULL DEFAULT now(),
+
+	CONSTRAINT transaction_pkey PRIMARY KEY (id),
+
+	CONSTRAINT transaction_sender_id_fkey FOREIGN KEY (sender_id)
+		REFERENCES public.user (id) MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION,
+
+	CONSTRAINT transaction_receiver_id_fkey FOREIGN KEY (receiver_id)
+		REFERENCES public.user (id) MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+) TABLESPACE pg_default;
+
+ALTER TABLE
+	IF EXISTS public.transaction OWNER to "MasterPostgres";
